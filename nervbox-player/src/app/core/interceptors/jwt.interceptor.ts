54652e70bb61d529bc4de8653063b5bb1716f533
ingel81 <ingel81@sgeht.net>
2026-01-05 18:09:56 +0100

    feat: auto-logout on 401 Unauthorized (invalid/expired token)

--- nervbox-player/src/app/core/interceptors/jwt.interceptor.ts
+++ nervbox-player/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,6 +1,10 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const snackBar = inject(MatSnackBar);
   const token = localStorage.getItem('nervbox_token');
 
   // Don't add token to auth endpoints
@@ -12,5 +16,17 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 && token) {
+        // Token is invalid - clear it and reload
+        localStorage.removeItem('nervbox_token');
+        snackBar.open('Session abgelaufen - bitte neu einloggen', 'OK', {
+          duration: 5000,
+        });
+        window.location.reload();
+      }
+      return throwError(() => error);
+    })
+  );
 };
